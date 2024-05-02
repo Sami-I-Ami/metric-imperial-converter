91189606669db230d989d852d6b746155cd06840
Sami Fink <fink.s.m.8651@gmail.com>
2024-05-02 00:18:30 +0000

    feat: completed convert handler class

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,42 +1,71 @@
 function ConvertHandler() {
+
+  const regex = /(\d+)?(\w+)/;
   
   this.getNum = function(input) {
-    let result;
-    
-    return result;
+    return input.match(regex)[1];
   };
   
   this.getUnit = function(input) {
-    let result;
-    
-    return result;
+    return input.match(regex)[2];
   };
   
   this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+    switch (initUnit) {
+      case 'gal':
+        return 'L';
+      case 'L':
+        return 'gal';
+      case 'lbs':
+        return 'kg';
+      case 'kg':
+        return 'lbs';
+      case 'mi':
+        return 'km';
+      case 'km':
+        return 'mi';
+    }
   };
 
   this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+    switch (initUnit) {
+      case 'gal':
+        return 'gallons';
+      case 'L':
+        return 'liters';
+      case 'lbs':
+        return 'pounds';
+      case 'kg':
+        return 'kilograms';
+      case 'mi':
+        return 'miles';
+      case 'km':
+        return 'kilometers';
+    }
   };
   
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    let result;
-    
-    return result;
+    switch (initUnit) {
+      case 'gal':
+        return initNum * galToL;
+      case 'L':
+        return initNum / galToL;
+      case 'lbs':
+        return initNum * lbsToKg;
+      case 'kg':
+        return initNum / lbsToKg;
+      case 'mi':
+        return initNum * miToKm;
+      case 'km':
+        return initNum / miToKm;
+    }
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+    return `${initNum} ${initUnit} converts to ${returnNum} ${returnUnit}`;
   };
   
 }
