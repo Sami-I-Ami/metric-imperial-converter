3a0292569f92bb8ac260eca2bed0cf14fcd88dd9
Sami Fink <fink.s.m.8651@gmail.com>
2024-05-03 09:53:18 +0000

    fix: missed ending ) for getNum call

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -7,9 +7,9 @@ let convertHandler = new ConvertHandler();
 suite('Unit Tests', function(){
     test('Reading Input', () => {
         assert.equal(convertHandler.getNum('3mi'), 3, 'whole number input');
-        assert.equal(convertHandler.getNum('3.1mi', 3.1, 'decimal number input'));
-        assert.equal(convertHandler.getNum('3/2mi', 1.5, 'fractional input'));
-        assert.equal(convertHandler.getNum('3.2/2mi', 1.6, 'fractional input with decimal'));
-        
+        assert.equal(convertHandler.getNum('3.1mi'), 3.1, 'decimal number input');
+        assert.equal(convertHandler.getNum('3/2mi'), 1.5, 'fractional input');
+        assert.equal(convertHandler.getNum('3.2/2mi'), 1.6, 'fractional input with decimal');
+
     })
 });
