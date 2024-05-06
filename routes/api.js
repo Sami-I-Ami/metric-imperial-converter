771f4b1c9a629141f26482f8d4c667cb2bcfbc2b
Sami Fink <fink.s.m.8651@gmail.com>
2024-05-06 18:40:10 +0000

    feat: added response for invalid inputs

--- routes/api.js
+++ routes/api.js
@@ -13,13 +13,20 @@ module.exports = function (app) {
     const initUnit = convertHandler.getUnit(input);
     const returnNum = convertHandler.convert(initNum, initUnit);
     const returnUnit = convertHandler.getReturnUnit(initUnit);
-    res.json({
-      initNum,
-      initUnit,
-      returnNum,
-      returnUnit,
-      string: convertHandler.getString(initNum, initUnit, returnNum, returnUnit)
-    })
+    if (initNum === 'invalid number' & initUnit === 'invalid unit') {
+      res.send('invalid number and unit');
+    } else if (initNum === 'invalid number') {
+      res.send('invalid number');
+    } else if (initUnit === 'invalid unit') {
+      res.send('invalid unit');
+    } else {
+      res.json({
+        initNum,
+        initUnit,
+        returnNum,
+        returnUnit,
+        string: convertHandler.getString(initNum, initUnit, returnNum, returnUnit)
+      });
+    }
   });
-
 };
