77dfebc7f30d20e47dd877486bc1f9f8f64dea98
Sami Fink <fink.s.m.8651@gmail.com>
2024-05-06 18:53:16 +0000

    feat: added functional test for valid input

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,21 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+  this.timeout(5000);
+  test('GET /api/convert with valid input', (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .get('/api/convert?input=10L')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.equal(res.body.initNum, 10);
+        assert.equal(res.body.initUnit, 'L');
+        assert.equal(res.body.returnNum, 2.64172);
+        assert.equal(res.body.returnUnit, 'gal');
+        assert.equal(res.body.string, '10 liters converts to 2.64172 gallons');
+        done();
+      });
+  })
 });
