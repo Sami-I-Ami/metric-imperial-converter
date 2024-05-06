472ff6c4b7e9dd3aeab843df4b6e40ff444a345d
Sami Fink <fink.s.m.8651@gmail.com>
2024-05-06 18:59:20 +0000

    feat: added rest of functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -22,5 +22,57 @@ suite('Functional Tests', function() {
         assert.equal(res.body.string, '10 liters converts to 2.64172 gallons');
         done();
       });
-  })
+  });
+  test('GET /api/convert with invalid unit input', (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .get('/api/convert?input=32g')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'text/html');
+        assert.equal(res.text, 'invalid unit');
+        done();
+      });
+  });
+  test('GET /api/convert with invalid number input', (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .get('/api/convert?input=3/7.2/4kg')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'text/html');
+        assert.equal(res.text, 'invalid number');
+        done();
+      });
+  });
+  test('GET /api/convert with completely invalid input', (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .get('/api/convert?input=3/7.2/4kilomegagram')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'text/html');
+        assert.equal(res.text, 'invalid number and unit');
+        done();
+      });
+  });
+  test('GET /api/convert with valid no number input', (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .get('/api/convert?input=kg')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.equal(res.body.initNum, 1);
+        assert.equal(res.body.initUnit, 'kg');
+        assert.equal(res.body.returnNum, 2.20462);
+        assert.equal(res.body.returnUnit, 'lbs');
+        assert.equal(res.body.string, '1 kilograms converts to 2.20462 pounds');
+        done();
+      });
+  });
 });
